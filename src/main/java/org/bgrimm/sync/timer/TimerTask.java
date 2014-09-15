6c527e6809acccb9e9e027bbcd26d8e382f316f6
daijun <junsmooth@163.com>
2014-09-15 23:02:44 +0800

    add AeroScout api demo to timertask

--- src/main/java/org/bgrimm/sync/timer/TimerTask.java
+++ src/main/java/org/bgrimm/sync/timer/TimerTask.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.aeroscout.mobileview.api.service.AssetAPIServicePortType;
+import com.aeroscout.mobileview.api.service.SystemAPIServicePortType;
+import com.aeroscout.mobileview.proxy.AeroScoutServiceLocator;
+
 @Component
 public class TimerTask {
 
@@ -21,4 +25,18 @@ public class TimerTask {
 		System.out.println(cityList);
 	}
 
+	@Scheduled(fixedDelay = 30000)
+	public void dataSync() {
+		String host = "";
+		int port = 802;
+		String user = "system";
+		String pass = "123456";
+		AeroScoutServiceLocator aeroScoutServiceLocator = new AeroScoutServiceLocator(
+				host, port, user, pass, false);
+
+		SystemAPIServicePortType systemServicePort = aeroScoutServiceLocator
+				.getSystemService();
+		AssetAPIServicePortType assetServicePort = aeroScoutServiceLocator
+				.getAssetService();
+	}
 }
